d8c178513348f6f375b48d2cff8451789c4fb47a
Jagger Wang <jaggerwang@gmail.com>
2018-04-19 13:59:28 +0800

    website iptables OUTPUT default ACCEPT

--- 17-website-iptables/sec.sh
+++ 17-website-iptables/sec.sh
@@ -6,20 +6,16 @@ iptables -X
 iptables -Z
 
 # 默认规则
-iptables -P  INPUT DROP
-iptables -P  OUTPUT DROP
+iptables -P INPUT DROP
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
 
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
-
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
-iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
-
 iptables -I INPUT -s 192.168.42.1 -p tcp --dport 22 -j ACCEPT
+
 iptables -I INPUT -s 192.168.42.0/24 -p tcp --dport 5901 -j ACCEPT
-iptables -A OUTPUT -m state --state ESTABLISHED -j ACCEPT
-
-
