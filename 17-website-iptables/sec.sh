5e46ffd740b8cf556fb0d8a5d77cb00a1fc70911
Aiden <luojin@simplecloud.cn>
2018-04-19 13:36:11 +0800

    修复 iptables OUTPUT 链问题

--- 17-website-iptables/sec.sh
+++ 17-website-iptables/sec.sh
@@ -6,22 +6,20 @@ iptables -X
 iptables -Z
 
 # 默认规则
-iptables -P   INPUT DROP
-# iptables -P  OUTPUT DROP
+iptables -P  INPUT DROP
+iptables -P  OUTPUT DROP
 
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
-iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
 
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 
-iptables -A INPUT -p tcp --dport 22 -j DROP
-iptables -I INPUT -s 192.168.42.1 -ptcp --dport 22 -j ACCEPT
-
-iptables -A INPUT -p tcp --dport 5901 -j DROP
-iptables -I INPUT -s 192.168.42.0/24 -ptcp --dport 5901 -j ACCEPT
+iptables -I INPUT -s 192.168.42.1 -p tcp --dport 22 -j ACCEPT
+iptables -I INPUT -s 192.168.42.0/24 -p tcp --dport 5901 -j ACCEPT
+iptables -A OUTPUT -m state --state ESTABLISHED -j ACCEPT
 
 
