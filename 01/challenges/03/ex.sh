f665304591907b8cc639ebac4e8c64e6c650fe64
kinglion580 <1609019405@qq.com>
2018-06-04 14:50:20 +0800

    Update ex.sh

--- 01/challenges/03/ex.sh
+++ 01/challenges/03/ex.sh
@@ -2,4 +2,4 @@
 
 # sudo ex -sc 'a|DEV_SERVER=https://dev.shiyanlou.com' -cx /etc/environment
 sudo ex -sc 'a|DEV_SERVER=https://dev.shiyanlou.com' -cx /etc/profile
-ex -sc 'DEV_ACCOUNT=shiyanlou' -cx ~/.zshrc
+ex -sc 'a|DEV_ACCOUNT=shiyanlou' -cx ~/.zshrc
