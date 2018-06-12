5e1fa065cd7e85918684b1bd568d5c368ebeb61a
Jagger Wang <jaggerwang@gmail.com>
2018-06-12 16:30:25 +0800

    opt docker related

--- old/50-docker-run-mysql/test_run.sh
+++ old/50-docker-run-mysql/test_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --env-file /home/shiyanlou/env_file -p 3306:3306 mysql:5.5
+docker run --name mysql -d --env-file /home/shiyanlou/env_file -p 3306:3306 mysql:5.5
--- old/51-update-prot-mapping-using-iptables/updateport.sh
+++ old/51-update-prot-mapping-using-iptables/updateport.sh
@@ -4,7 +4,8 @@ old_host_port=3306
 new_host_port=3305
 dst_port=3306
 
-mysql_container_ip=`docker inspect -f {{.NetworkSettings.IPAddress}} mysql_server`
+# 命令最后的 mysql 为 MySQL 服务容器的名称，请依据实际情况修改。
+mysql_container_ip=`docker inspect -f {{.NetworkSettings.IPAddress}} mysql`
 
 sudo iptables -t nat -A DOCKER ! -i docker0 -p tcp --dport $new_host_port -j DNAT --to-destination $mysql_container_ip:$dst_port
 sudo iptables -t nat -D DOCKER ! -i docker0 -p tcp --dport $old_host_port -j DNAT --to-destination $mysql_container_ip:$dst_port
