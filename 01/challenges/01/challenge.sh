a86205509f5f2c8be282b79c4c30cffbf191f3b2
yuanchunrong <2199553410@qq.com>
2020-04-23 10:16:41 +0800

    补充第一周第一个挑战的答案

--- 01/challenges/01/challenge.sh
+++ 01/challenges/01/challenge.sh
@@ -7,3 +7,7 @@ echo 'DEV_SERVER=https://dev.shiyanlou.com' | sudo tee -a /etc/profile
 
 # 修改用户自己的文件不需要特权
 echo 'DEV_ACCOUNT=shiyanlou' >> ~/.zshrc
+
+# 实验环境中，shiyanlou 用户默认使用的是 zsh。需要注意 zsh 不使用 /etc/profile 文件，而是使用 /etc/zsh 下面的 zshenv、zprofile、zshrc、zlogin 文件，并以这个顺序进行加载。
+# 大家如果直接 source /etc/profile ，会造成 zsh 主题丢失。
+# 一个解决办法是：在 ~/.zshrc 文件的第一行加上 source /etc/profile，这样就可以使环境变量立即生效，同时 zsh 主题配置不会丢失。
